e506dbe7aa8ccf4ba135713615a4bee2f0535c80
JackyLeg <91046882+JackyLeg@users.noreply.github.com>
2022-10-19 17:39:37 +0700

    menambahkan file prak

--- Biodata.java
+++ Biodata.java
@@ -1,91 +1,147 @@
-import javax.swing.*;
+//package praktikum.java.netbeans;
 import java.awt.event.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Enumeration;
+import java.io.*;
+import javax.swing.*;
 
+public class Biodata extends javax.swing.JFrame {
 
-public class Biodata extends JFrame implements ActionListener {
-    JPanel biodataPanel;
-    JTextField tfNama; //tf = textfield
-    JTextArea tArea; //t = text
-    JRadioButton lakiLakiRadioButton;
-    JRadioButton perempuanRadioButton;
-    JCheckBox pythonCheckBox;
-    JCheckBox javaCheckBox;
-    JButton printButton;
-    JComboBox comboHobi;
-    JLabel l1, l2, l3, l4, l5, hasilPrint; //l = label
-    ButtonGroup gender;
-
-    Biodata() {
-        JFrame frame = new JFrame("Biodata (I Made Novandy Asardi - 064002100030)");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setSize(800,500);
-        frame.setLayout(null);
-        frame.setVisible(true);
-        String hobi[]={"(Pilih)", "Coding", "Memasak", "Membaca", "Menonton", "Belajar"};
-
-        l1 = new JLabel("Nama: "); l1.setBounds(20,50,150,20);
-        l2 = new JLabel("Hobi: "); l2.setBounds(20,100,150,20);
-        l3 = new JLabel("Alamat: "); l3.setBounds(20,150,150,20);
-        l4 = new JLabel("Jenis Kelamin: "); l4.setBounds(20,270,150,20);
-        l5 = new JLabel("Skill apa yang kamu miliki?"); l5.setBounds(20,330,300,20);
-        frame.add(l1); frame.add(l2); frame.add(l3); frame.add(l4); frame.add(l5);
-
-        tfNama = new JTextField(""); tfNama.setBounds(60,50,150,20);
-        comboHobi = new JComboBox(hobi);/*mengambil dari var array hobi*/comboHobi.setBounds(60,100,150,20);
-        tArea = new JTextArea(""); tArea.setBounds(80,150,200,100);
-        frame.add(tfNama); frame.add(comboHobi); frame.add(tArea);
-
-        lakiLakiRadioButton = new JRadioButton("Laki-laki"); lakiLakiRadioButton.setBounds(20,300,82,20);
-        lakiLakiRadioButton.setActionCommand("Laki-laki");
-        perempuanRadioButton = new JRadioButton("Perempuan"); perempuanRadioButton.setBounds(100,300,150,20);
-        perempuanRadioButton.setActionCommand("Perempuan");
-        frame.add(lakiLakiRadioButton); frame.add(perempuanRadioButton);
-
-        pythonCheckBox = new JCheckBox("Python"); pythonCheckBox.setBounds(20,350,100,20);
-        javaCheckBox = new JCheckBox("Java"); javaCheckBox.setBounds(120,350,150,20);
-        frame.add(pythonCheckBox); frame.add(javaCheckBox);
-
-        printButton = new JButton("Print"); printButton.setBounds(60,380,150,20);
-        hasilPrint = new JLabel(""); hasilPrint.setBounds(20,390,500,300);
-        frame.add(printButton); frame.add(hasilPrint);
-
-        printButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String nama, hobi, alamat, jk, output;
-                nama = tfNama.getText();
-                hobi = comboHobi.getSelectedItem().toString();
-                alamat = tArea.getText();
-                gender = new ButtonGroup(); //agar radio button berfungsi
-                gender.add(lakiLakiRadioButton);
-                gender.add(perempuanRadioButton);
-                jk = getSelectedButtonText(gender);
-                output = "Nama saya " + nama + " dan hobi saya adalah " + hobi +
-                        ". \nSaya tinggal di " + alamat
-                        + ". Saya " + jk + ".";
-                hasilPrint.setText(String.valueOf(output));
-            }
-        });   
+    public Biodata() {
+        initComponents();
+        this.setTitle("File Processing");
     }
-    public void actionPerformed(ActionEvent e) {
-        String s1 = tfNama.getText();
-        String s2 = tArea.getText();
-    }
-    public String getSelectedButtonText(ButtonGroup buttonGroup) {
-        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
-            AbstractButton button = buttons.nextElement();
 
-            if (button.isSelected()) {
-                return button.getText();
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        nama = new javax.swing.JLabel();
+        nim = new javax.swing.JLabel();
+        hobi = new javax.swing.JLabel();
+        tfNama = new javax.swing.JTextField();
+        tfNim = new javax.swing.JTextField();
+        tfHobi = new javax.swing.JTextField();
+        show = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        nama.setText("Nama =");
+
+        nim.setText("NIM =");
+
+        hobi.setText("Hobi = ");
+
+        tfNama.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfNamaActionPerformed(evt);
             }
+        });
+
+        tfHobi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfHobiActionPerformed(evt);
+            }
+        });
+
+        show.setText("Input Data!");
+        show.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                showActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Delete Data :(");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(hobi)
+                        .addGap(18, 18, 18)
+                        .addComponent(tfHobi))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nama)
+                            .addComponent(nim))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(tfNama)
+                            .addComponent(tfNim, javax.swing.GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(show, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(nama)
+                    .addComponent(tfNama, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(45, 45, 45)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nim)
+                    .addComponent(tfNim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(hobi)
+                    .addComponent(tfHobi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(show, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void tfNamaActionPerformed(java.awt.event.ActionEvent evt) {                                       
+        // TODO add your handling code here:
+    }                                      
+
+    private void tfHobiActionPerformed(java.awt.event.ActionEvent evt) {                                       
+        // TODO add your handling code here:
+    }                                      
+
+    private void showActionPerformed(java.awt.event.ActionEvent evt) {                                     
+        JOptionPane.showMessageDialog(null, "Hore! Datamu berhasil masuk!", "Success", 1);
+        try(FileWriter fw = new FileWriter("input.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("Nama: " + tfNama.getText() +
+                    "\nNIM: " + tfNim.getText() + 
+                    "\nHobi: " + tfHobi.getText());
         }
-        return null;
+        catch (IOException e) {
+            System.out.println("Terjadi kesalahan.");
+            e.printStackTrace();
+        }
+    }                                    
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Biodata().setVisible(true);
+            }
+        });
     }
-    public static void main(String[] args){
-        new Biodata();
-    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JLabel hobi;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel nama;
+    private javax.swing.JLabel nim;
+    private javax.swing.JButton show;
+    private javax.swing.JTextField tfHobi;
+    private javax.swing.JTextField tfNama;
+    private javax.swing.JTextField tfNim;
+    // End of variables declaration                   
 }
